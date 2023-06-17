dc96806f2cf79cbb111d9f465e39e18b8af43229
bboysidou <bedrani.sidali.94@gmail.com>
2023-06-17 21:46:24 +0100

    Add connection to android phones and necessery linux stuff

--- post_install.sh
+++ post_install.sh
@@ -1,4 +1,4 @@
-sudo pacman -S lxappearance picom nitrogen rofi chromium polybar neovim zsh lightdm lightdm-slick-greeter nodejs npm ntfs-3g docker docker-compose nautilus alsa-utils alsa-plugins alsa-lib alsa-firmware unzip
+sudo pacman -S lxappearance picom nitrogen rofi chromium polybar neovim zsh lightdm lightdm-slick-greeter nodejs npm ntfs-3g docker docker-compose nautilus alsa-utils alsa-plugins alsa-lib alsa-firmware unzip ripgrep gvfs-mtp
 
 # ENABLE LIGHTDM ON START AND UPDATE GRETTER
 sudo systemctl enable lightdm.service
@@ -8,8 +8,14 @@ sudo systemctl enable lightdm.service
 # MAKE DOCKER RUN WITHOUT SUDO
 sudo usermod -aG docker $USER
 
+# BLUETOOTH
+sudo pacman -S bluez bluez-utils blueberry
+
+# FIRMWARE AND LINUX HEADERS
+sudo pacman -S linux-firmware linux-firmware-marvell linux-firmware-qlogic linux linux-headers mesa-utils
+
 # IF GPU IS NVIDIA
-sudo pacman -S nvidia nvidia-utils nvidia-settings
+sudo pacman -S nvidia nvidia-utils nvidia-settings nvidia-dkms
 
 # VIDEO AUDIO CODECS
 sudo pacman -S a52dec faac faad2 flac jasper lame libdca libdv libmad libmpeg2 libtheora libvorbis libxv wavpack x264 xvidcore vlc
