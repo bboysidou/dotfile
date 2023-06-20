786bcf955732be46f3664bb9c85faa77735d79b5
bboysidou <bedrani.sidali.94@gmail.com>
2023-06-20 23:30:23 +0100

    ADD TMUX CONFIG

--- post_install.sh
+++ post_install.sh
@@ -1,8 +1,9 @@
-sudo pacman -S lxappearance picom nitrogen rofi chromium polybar neovim zsh lightdm lightdm-slick-greeter nodejs npm ntfs-3g docker docker-compose nautilus alsa-utils alsa-plugins alsa-lib alsa-firmware unzip ripgrep gvfs-mtp
+sudo pacman -S lxappearance picom nitrogen rofi chromium polybar neovim zsh tmux lightdm lightdm-slick-greeter nodejs npm ntfs-3g docker docker-compose nautilus alsa-utils alsa-plugins alsa-lib alsa-firmware unzip ripgrep gvfs-mtp
 
 # ENABLE LIGHTDM ON START AND UPDATE GRETTER
 sudo systemctl enable lightdm.service
 
+# CHNAGE THE GREETER TO lightdm-slick-greeter
 # -- sudo vim /etc/lightdm/lightdm.conf
 
 # MAKE DOCKER RUN WITHOUT SUDO
@@ -26,6 +27,7 @@ sudo pacman -S a52dec faac faad2 flac jasper lame libdca libdv libmad libmpeg2 l
 # 3- Powerlevel10k
 # 4- zsh-autosuggestions
 # 5- zsh-syntax-highlighting
+# 6- tmux plugin manager (tpm)
 
 # FROM AUR (YAY) INSTALL i3lock
 yay -S i3lock-color
