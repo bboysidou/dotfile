90dcdbbb9ec9d0160f95a7fc2fffe14639bc9dc8
bboysidou <bedrani.sidali.94@gmail.com>
2023-09-30 16:28:20 +0100

    adding powermenu

--- rofi/powermenu/powermenu.sh
+++ rofi/powermenu/powermenu.sh
@@ -1,4 +1,4 @@
-#!/usr/bin/env bash
+#!/bin/sh
 dir="$HOME/.config/rofi/powermenu/type"
 theme='style'
 
@@ -22,46 +22,15 @@ rofi_cmd() {
 		-mesg "Uptime: $uptime" \
 		-theme ${dir}/${theme}.rasi
 }
+chosen=$(printf "  Power Off\n  Restart\n  Lock" | rofi -dmenu \
+		-p "Uptime: $uptime" \
+		-mesg "Uptime: $uptime" \
+		-theme ${dir}/${theme}.rasi
+)
 
-# Confirmation CMD
-confirm_cmd() {
-	rofi -dmenu \
-		-p 'Confirmation' \
-		-mesg 'Are you Sure?' \
-		-theme ${dir}/shared/confirm.rasi
-}
-
-# Ask for confirmation
-confirm_exit() {
-	echo -e "$yes\n$no" | confirm_cmd
-}
-
-# Pass variables to rofi dmenu
-run_rofi() {
-	echo -e "$shutdown\n$reboot" | rofi_cmd
-}
-
-# Execute Command
-run_cmd() {
-	selected="$(confirm_exit)"
-	if [[ "$selected" == "$yes" ]]; then
-		if [[ $1 == '--shutdown' ]]; then
-			systemctl poweroff
-		elif [[ $1 == '--reboot' ]]; then
-			systemctl reboot
-		fi
-	else
-		exit 0
-	fi
-}
-
-# Actions
-chosen="$(run_rofi)"
-case ${chosen} in
-    $shutdown)
-		run_cmd --shutdown
-        ;;
-    $reboot)
-		run_cmd --reboot
-        ;;
+case "$chosen" in
+	"  Power Off") poweroff ;;
+	"  Restart") reboot ;;
+	"  Lock") sh $HOME/.config/screen-lock/lock.sh;;
+	*) exit 1 ;;
 esac
